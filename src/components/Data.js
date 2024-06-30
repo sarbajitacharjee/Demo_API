3d5e8b8160381b4c32b849bce2b92c4305738d34
Sarbajit Acharjee <sarbajitacharjee712@gmail.com>
2024-07-01 01:31:57 +0530

    profile photoa added

--- src/components/Data.js
+++ src/components/Data.js
@@ -43,30 +43,32 @@ const DataTable = () => {
     <>
 
 
-    <div className="overflow-x-auto">
+    {/* <div className="overflow-x-auto"> */}
       <table className="min-w-full bg-white border border-gray-300">
         <thead>
           <tr>
-            <th className="px-4 py-2 border">ID</th>
-            <th className="px-4 py-2 border">Username</th>
-            <th className="px-4 py-2 border">Profile URL</th>
+            <th className="px-4 py-2 border text-black">ID</th>
+            <th className="px-4 py-2 border text-black">Username</th>
+            <th className="px-4 py-2 border text-black">Profile URL</th>
+            <th className="px-4 py-2 border text-black">Profile Picture</th>
           </tr>
         </thead>
         <tbody>
           {data.map((user) => (
             <tr key={user.id}>
-              <td className="px-4 py-2 border text-black">{user.id}</td>
-              <td className="px-4 py-2 border text-black">{user.login}</td>
-              <td className="px-4 py-2 border text-black">
+              <td className="px-4 py-2 border text-black text">{user.id}</td>
+              <td className="px-4 py-2 border text-black text-3xl">{user.login}</td>
+              <td className="px-4 py-2 border text-black ">
                 <a href={user.html_url} className="text-blue-500" target="_blank" rel="noopener noreferrer">
                   {user.html_url}
                 </a>
               </td>
+              <td className='px-3 py-2'> <img src={user.avatar_url} width={300} height={300}></img> </td>
             </tr>
           ))}
         </tbody>
       </table>
-    </div>
+    {/* </div> */}
     </>
   );
 };
